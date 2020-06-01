1ced2ea25fd5a0eddba54ff625d3a2f3a62d6c54
黎若楠 <liruonan@xinhua.org>
2020-06-01 11:43:26 +0800

    add react test

--- test.js
+++ test.js
@@ -3,16 +3,15 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = 'http://hanabi.data-viz.cn/visualisation/line_chart'
-  await page.goto('http://192.168.1.4:8008');
+  const vueURL = 'http://172.16.168.96:8080/'
+  await page.goto(vueURL);
   const dimensions = await page.$eval('#container', el => {
     const containerBc = el.getBoundingClientRect()
-    console.log('containerBc', containerBc)
     return {
       top: containerBc.top,
       left: containerBc.left,
       width: containerBc.width,
-      height: containerBc.height || 500
+      height: containerBc.height
     }
   })
 
@@ -20,34 +19,47 @@ const puppeteer = require('puppeteer');
     for (let i = 0; i < msg.args().length; ++i)
     console.log(`${i}: ${msg.args()[i]}`); // 译者注：这句话的效果是打印到你的代码的控制台
   });
-
-  setTimeout(async() => {
-    await screenShot(page, browser, dimensions)
-    await browser.close()
-  }, 5000)
+  screenShotStart(page, dimensions, browser)
 })();
 
-
-async function screenShot(page, browser, dimensions) {
-  await new Promise((resolve, reject) => {
-    var i = 0;
-    const timer = setInterval(async() => {
-      if (i >= 60) {
-        clearInterval(timer)
-        resolve()
-      } else {
-        i++
-        await page.screenshot({ 
-          path: `example-${i}.png`,
-          clip: {
-            x: dimensions.left,
-            y: dimensions.top,
-            width: dimensions.width,
-            height: dimensions.height
-          }
-        })
-      }
-    }, 1200)
+async function screenShotStart(page, dimensions, browser) {
+  const start = await page.evaluate(() => {
+    // 使用 window.snapStart 开始
+    return window.snapStart
   })
+  if (start) { 
+    await screenShot(page, dimensions)
+    await browser.close()
+  } else {
+    setTimeout(() => {
+      screenShotStart(page, dimensions, browser)
+    }, 500)
+  }
 }
 
+async function screenShot(page, dimensions) {
+  await new Promise((resolve, reject) => {
+    var i = 0;
+    var timer = setInterval(async() => {
+      i++
+      await page.screenshot({ 
+        path: `./outImages-vue/example-${i}.png`,
+        clip: {
+          x: dimensions.left,
+          y: dimensions.top,
+          width: dimensions.width,
+          height: dimensions.height || 500
+        }
+      })
+
+      const end = await page.evaluate(() => {
+        // 使用 window.snapEnd 结束
+        return window.snapEnd
+      })
+      if (end) {
+          clearInterval(timer)
+          resolve()
+      }
+    }, 500)
+  })
+}
--- test1.js
+++ test1.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = 'http://hanabi.data-viz.cn/visualisation/line_chart'
-  await page.goto('http://192.168.1.4:8008');
+  const reactURL = 'http://172.16.168.96:3000/#/test'
+  await page.goto(reactURL);
   const dimensions = await page.$eval('#container', el => {
     const containerBc = el.getBoundingClientRect()
     return {
@@ -19,13 +19,23 @@ const puppeteer = require('puppeteer');
     for (let i = 0; i < msg.args().length; ++i)
     console.log(`${i}: ${msg.args()[i]}`); // 译者注：这句话的效果是打印到你的代码的控制台
   });
-
-  setTimeout(async() => {
-    await screenShot(page, dimensions)
-    await browser.close()
-  }, 5000)
+  screenShotStart(page, dimensions, browser)
 })();
 
+async function screenShotStart(page, dimensions, browser) {
+  const start = await page.evaluate(() => {
+    // 使用 window.snapStart 开始
+    return window.snapStart
+  })
+  if (start) { 
+    await screenShot(page, dimensions)
+    await browser.close()
+  } else {
+    setTimeout(() => {
+      screenShotStart(page, dimensions, browser)
+    }, 500)
+  }
+}
 
 async function screenShot(page, dimensions) {
   await new Promise((resolve, reject) => {
@@ -33,7 +43,7 @@ async function screenShot(page, dimensions) {
     var timer = setInterval(async() => {
       i++
       await page.screenshot({ 
-        path: `example-${i}.png`,
+        path: `./outImages-react/example-${i}.png`,
         clip: {
           x: dimensions.left,
           y: dimensions.top,
@@ -42,15 +52,14 @@ async function screenShot(page, dimensions) {
         }
       })
 
-      const snaped = await page.evaluate(() => {
+      const end = await page.evaluate(() => {
         // 使用 window.snapEnd 结束
         return window.snapEnd
       })
-      if (snaped) {
+      if (end) {
           clearInterval(timer)
           resolve()
       }
-    }, 1200)
+    }, 1000)
   })
 }
-
